ed2b7eee1396cb85b25b66897eee12887438e582
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2025-11-07 23:00:02 +0530

    removed string formatted html

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ function deleteTodo(index) {
   let delElement = document.querySelector(".todo-" + index);
   delElement.parentNode.removeChild(delElement);
 
-  if(ctr === index) {
+
   ctr = ctr - 1;  
-  }
+
+
 
 }
 
@@ -19,15 +20,28 @@ function addTodo() {
         vare.setAttribute("class", "clear");
         let parentDelete = document.querySelector(".wrapper").appendChild(vare);
     }
+    ctr = ctr + 1;
     let addElement = document.querySelector(".input");
     let value = addElement.value;
+
+    let textEl = document.createElement("h3");
+    let buttonEl = document.createElement("button");
+
+    textEl.innerHTML = ctr + ' . ' + value;
+
+    buttonEl.setAttribute("onclick", "deleteTodo(" + ctr + ")");
+    buttonEl.innerHTML = "Delete";
+    
     
     let El = document.createElement("div");
-    ctr = ctr + 1;
+    El.setAttribute("class", "flex todo-" + ctr);
+
+    El.appendChild(textEl);
+    El.appendChild(buttonEl);
 
     let newEl = document.querySelector(".clear").appendChild(El);
 
-    newEl.innerHTML = '<div class="flex todo-' + ctr + '"> <h3>' + ctr + '. ' + value + '</h3><button onclick="deleteTodo(' + ctr + ')">Delete</button></div>'
+    // newEl.innerHTML = '<div class="flex todo-' + ctr + '"> <h3>' + ctr + '. ' + value + '</h3><button onclick="deleteTodo(' + ctr + ')">Delete</button></div>'
 }
 
 function clearTodo() {
